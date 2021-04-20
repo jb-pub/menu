c457751791485ea0ea78c98fb7a646820008fea8
Jean Claude Correale <jeanclaudecorreale@gmail.com>
2021-04-20 05:35:10 +0200

    Refactored reader2.py

--- excel/reader2.py
+++ excel/reader2.py
@@ -1,6 +1,6 @@
 def find_last_items_row(ws, start):
     i = start
-    while  i < ws.max_row:
+    while i < ws.max_row:
         val = ws[f'C{i}'].value
         if not val:
             return i
@@ -9,12 +9,58 @@ def find_last_items_row(ws, start):
 
 def find_last_ref_col(ws, start):
     i = start
-    while  i < ws.max_column:
-        val = ws.cell(3, start).value
+    while i < ws.max_column:
+        # val = ws.cell(3, start).value
+        val = ws.cell(3, i).value
         if not val:
             return i
         i += 1
     return ws.max_column
+
+def col_next_cell(ws, col, startRow):
+    r = startRow
+    while r < ws.max_row:
+        val = ws.cell(r, col).value
+        if val:
+            return r
+        r += 1
+    return -1
+
+
+def append_subcategories(ws, bIndex, subcategories):
+    cIndex = bIndex + 1
+    subcatName = ws[f'B{bIndex}'].value
+    subcategory = { "name": subcatName, "items": [] }
+    subcategories.append(subcategory)
+    last_items_row = find_last_items_row(ws, cIndex)
+
+    for i in range(cIndex, last_items_row):
+        itemName = ws[f'C{cIndex}'].value
+        itemDescr = ws[f'D{cIndex}'].value
+        menuItem = { "name": itemName, "description": itemDescr, "allergens": [], "refs": []}
+        subcategory["items"].append(menuItem)
+        cIndex += 1
+        # TODO Allergens vs infos
+        for k in range(0, 17):
+            allergIndex = 5 + k
+            allergValue = ws.cell(i, allergIndex).value
+            if allergValue:
+                allergName = ws.cell(4, allergIndex).value
+                menuItem["allergens"].append(allergName)
+
+        firstRefCol = 18 + 4
+        lastRefCol = find_last_ref_col(ws, firstRefCol)
+        # print(openpyxl.utils.cell.get_column_letter(lastRefCol))
+
+        for k in range(firstRefCol, lastRefCol, 3):
+            ref = {
+                "ref": ws.cell(i, k).value,
+                "size": ws.cell(i, k + 1).value,
+                "price": ws.cell(i, k + 2).value
+            }
+
+            if ref["ref"]:
+                menuItem["refs"].append(ref)
 
 import openpyxl
 import yaml
@@ -24,59 +70,18 @@ from openpyxl import load_workbook
 
 wb = load_workbook(filename = 'DATABASE_MENU_VENDITA.xlsx', data_only=True)
 
-result = {}
-
-result["categories"] = []
-
-catIndex = 0 # TODO
+result = { "categories": [] }
 
 for sheet in wb.sheetnames:
-    result["categories"].append({ "name": sheet, "subcategories": [] })
+    category = { "name": sheet, "subcategories": [] }
+    result["categories"].append(category)
 
     # subcatIndex = 0
     bIndex = 4
-    cIndex = 5
 
-    if sheet == "BIRRE": # TODO
-        ws = wb[sheet]
+    ws = wb[sheet]
 
-        subcatName = ws[f'B{bIndex}'].value
-
-        subcategories = result["categories"][catIndex]["subcategories"]
-        subcategory = { "name": subcatName, "items": [] }
-        subcategories.append(subcategory)
-        
-        last_items_row = find_last_items_row(ws, 5)
-
-        for i in range(cIndex, last_items_row):
-            itemName = ws[f'C{cIndex}'].value
-            itemDescr = ws[f'D{cIndex}'].value
-            menuItem = { "name": itemName, "description": itemDescr, "allergens": [], "refs": []}
-            subcategory["items"].append(menuItem)
-            cIndex += 1
-
-            for k in range(0, 17):
-                allergIndex = 5 + k
-                allergValue = ws.cell(i, allergIndex).value
-                if allergValue:
-                    allergName = ws.cell(4, allergIndex).value
-                    menuItem["allergens"].append(allergName)
-
-            firstRefCol = 18 + 4
-            lastRefCol = find_last_ref_col(ws, firstRefCol)
-            # print(openpyxl.utils.cell.get_column_letter(lastRefCol))
-
-            for k in range(firstRefCol, lastRefCol, 3):
-                ref = {
-                    "ref": ws.cell(i, k).value,
-                    "size": ws.cell(i, k + 1).value,
-                    "price": ws.cell(i, k + 2).value
-                }
-
-                if ref["ref"]:
-                    menuItem["refs"].append(ref)
-
-        cIndex = last_items_row
+    append_subcategories(ws, bIndex, category["subcategories"])
 
 yamlResult = yaml.dump(result)
 jsonResult = json.dumps(result)
