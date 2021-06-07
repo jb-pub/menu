9513cac8a063fa3d432da1caef98620849257e2e
Jean Claude Correale <jeanclaudecorreale@gmail.com>
2021-06-07 10:01:23 +0200

    Fixed only on last items

--- excel/reader2.py
+++ excel/reader2.py
@@ -5,7 +5,7 @@ def find_last_items_row(ws, start):
         if not val:
             return i
         i += 1
-    return ws.max_row
+    return ws.max_row + 1
 
 def find_last_ref_col(ws, start):
     i = start
@@ -38,7 +38,7 @@ def append_subcategories(ws, cIndex, subcategories):
     #print (subcatName)
     #print (last_items_row)
 
-    for i in range(eIndex, last_items_row + 1):
+    for i in range(eIndex, last_items_row):
         itemId = ws[f'F{eIndex}'].value
         itemName = ws[f'E{eIndex}'].value
         itemDescr = ws[f'G{eIndex}'].value
